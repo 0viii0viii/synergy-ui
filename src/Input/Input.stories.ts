cefecbe42ef77270192e0e6c8271ee0a5b40fb0c
양다민 <cxz26@innogrid.com>
2024-08-14 15:34:17 +0900

    refactor: story & 스타일 수정

--- src/Input/Input.stories.ts
+++ src/Input/Input.stories.ts
@@ -10,6 +10,41 @@ const meta: Meta<typeof Input> = {
 export default meta;
 type Story = StoryObj<typeof Input>;
 
-const Template: Story = {};
+const Template: Story = {
+	argTypes: {
+		type: {
+			table: {
+				type: {
+					summary: 'HTMLInputTypeAttribute',
+					detail:
+						'"button"\n' +
+						'| "checkbox"\n' +
+						'| "color"\n' +
+						'| "date"\n' +
+						'| "datetime-local"\n' +
+						'| "email"\n' +
+						'| "file"\n' +
+						'| "hidden"\n' +
+						'| "image"\n' +
+						'| "month"\n' +
+						'| "number"\n' +
+						'| "password"\n' +
+						'| "radio"\n' +
+						'| "range"\n' +
+						'| "reset"\n' +
+						'| "search"\n' +
+						'| "submit"\n' +
+						'| "tel"\n' +
+						'| "text"\n' +
+						'| "time"\n' +
+						'| "url"\n' +
+						'| "week"\n' +
+						'| (string & {})',
+				},
+			},
+			control: 'text',
+		},
+	},
+};
 
 export const Default: Story = Template;
