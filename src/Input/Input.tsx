b2f4e7c2d24cb4297adb27b4440a7e1f3cb15cfd
양다민 <cxz26@innogrid.com>
2024-08-14 15:38:14 +0900

    refactor: className 으로 스타일 커스텀 할 수 있도록 수정

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,7 +1,11 @@
+import classNames from 'classnames/bind';
+
 import styles from './styles.module.scss';
+
+const cx = classNames.bind(styles);
 
 export type TextFieldProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = ({ ...props }: TextFieldProps) => {
-	return <input className={styles.input} {...props} />;
+export const Input = ({ className, ...props }: TextFieldProps) => {
+	return <input className={cx(styles.input, className)} {...props} />;
 };
